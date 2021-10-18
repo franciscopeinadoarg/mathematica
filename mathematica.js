def103e0a0da9c045d3aa4bfc3efb04f3b5a8264
franciscopeinadoarg <88398141+franciscopeinadoarg@users.noreply.github.com>
2021-10-18 09:45:44 -0300

    Add files via upload

---
+++ mathematica.js
@@ -0,0 +1,41 @@
+
+function subtract(x, y) { 
+    var menos = x - y;
+    return menos;
+}
+//✔ 
+
+function sum(x, y) {
+    var r = x + y;
+    return r;
+} 
+//✔
+
+function numrandom(min, max) {  
+   var az2 = Math.random() * (max - min) + min;
+    return Math.floor(az2);
+
+};  
+//✔
+
+
+function div(factor1, factor2){ 
+   var res = factor1 / factor2; 
+   return res;
+}
+//✔ 
+
+function mult(factor1, factor2) { 
+    var resl = factor1 * factor2; 
+    return resl;
+}
+//✔
+
+function plus(factor1, factor2){ 
+    var resl2 = factor1 % factor2; 
+    return resl2;
+}
+//✔
+
+import {  } from "module";
+
